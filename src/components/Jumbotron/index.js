ad876c602265050654fff647c60ebd41daa9f785
Zach Corpuz <zachcorpuz@yahoo.com>
2020-09-02 14:08:37 -0700

    Added Jumbotron JS

---
+++ src/components/Jumbotron/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Jumbotron = ({ children }) => {
+  return (
+    <div
+      style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
+      className="jumbotron"
+    >
+      {children}
+    </div>
+  );
+}
+
+export default Jumbotron;
